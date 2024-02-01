eb75706801cb1a950085f44dd83e1377cbfb8828
QuantoPrizm <qweasdzxcqazwsxedc16@mail.ru>
2024-02-01 20:10:10 +0400

    Update bullet_1.gd

--- game_project/Scenes/Bullet1/bullet_1.gd
+++ game_project/Scenes/Bullet1/bullet_1.gd
@@ -7,6 +7,8 @@ var gravity = 10 # Значение гравитации
 
 func _ready():
 	linear_velocity = Vector2(x_speed, y_speed) # Устанавливаем начальную скорость пули
+	$image.rotation = linear_velocity.angle() # Поворачиваем спрайт в соответствии с направлением скорости
 
 func _integrate_forces(state):
 	linear_velocity.y += gravity * state.get_step() # Применяем гравитацию к скорости по оси Y
+	$image.rotation = linear_velocity.angle() # Обновляем поворот спрайта в соответствии с изменением скорости
